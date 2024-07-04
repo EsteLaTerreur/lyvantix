d0d80b09a6d03aa24f6c3234b9b783cbb32e11d4
EsteLaTerreur <139997966+EsteLaTerreur@users.noreply.github.com>
2024-07-04 11:30:49 +0200

    Update scripts.js

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,6 +1,6 @@
 // constantes 
 var couleur_du_cache = 'black';
-var cheminFichierTexte = 'texte8.txt';
+var cheminFichierTexte = 'texte6.txt';
 
 // traitement texte
 function traitement_txt(contenu) {
